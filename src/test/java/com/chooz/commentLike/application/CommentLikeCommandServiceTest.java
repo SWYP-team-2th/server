4694cb60f4fb9b27febfa570256dd100bb11c06d
yunseongoh <gagizzang92@gmail.com>
2025-06-15 18:31:55 +0900

    fix : 컨트롤러 path 수정

---
+++ src/test/java/com/chooz/commentLike/application/CommentLikeCommandServiceTest.java
@@ -0,0 +1,52 @@
+package com.chooz.commentLike.application;
+
+import com.chooz.comment.domain.Comment;
+import com.chooz.commentLike.domain.CommentLike;
+import com.chooz.commentLike.domain.CommentLikeRepository;
+import com.chooz.support.IntegrationTest;
+import com.chooz.support.fixture.CommentFixture;
+import com.chooz.support.fixture.UserFixture;
+import com.chooz.user.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CommentLikeServiceTest extends IntegrationTest {
+
+    @Autowired
+    private CommentLikeRepository commentLikeRepository;
+
+    @Autowired
+    private CommentLikeService commentLikeService;
+
+    @Test
+    @DisplayName("댓글좋아요 생성")
+    void createCommentLike() {
+        //given, when
+        CommentLike commentLike = createAndGetSavedCommentLike();
+        //then
+        assertThat(commentLike).isNotNull();
+    }
+
+    @Test
+    void deleteCommentLike() {
+        //given
+        CommentLike commentLike = createAndGetSavedCommentLike();
+
+        // when
+        commentLikeService.deleteLikeComment(commentLike.getCommentId(), commentLike.getUserId());
+
+        // then
+        assertThat(commentLikeRepository.findById(commentLike.getId())).isEmpty();
+    }
+
+    private CommentLike createAndGetSavedCommentLike() {
+        Comment comment = CommentFixture.createCommentBuilder().id(1L).build();
+        User user = UserFixture.createUserBuilder().id(1L).build();
+        commentLikeService.createLikeComment(comment.getId(), user.getId());
+        return commentLikeRepository
+                .findByCommentIdAndUserId(comment.getId(), user.getId()).orElseThrow();
+    }
+}
