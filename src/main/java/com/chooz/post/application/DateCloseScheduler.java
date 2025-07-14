5c6e6b4b11254372d078e01eff27b2142ca39d95
wlsh44 <k87913j@naver.com>
2025-07-14 11:24:22 +0900

    fix: 스케줄링 transactional 빠진 부분 추가

--- src/main/java/com/chooz/post/application/DateCloseScheduler.java
+++ src/main/java/com/chooz/post/application/DateCloseScheduler.java
@@ -3,18 +3,26 @@ package com.chooz.post.application;
 import com.chooz.post.domain.Post;
 import com.chooz.post.domain.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DateCloseScheduler {
 
     private final PostRepository postRepository;
 
+    @Transactional
     @Scheduled(fixedDelay = 1000 * 60)
     public void closePostsByDate() {
-        postRepository.findPostsNeedToClose()
-                .forEach(Post::close);
+        log.info("마감 스케줄링 시작");
+        List<Post> postsNeedToClose = postRepository.findPostsNeedToClose();
+        postsNeedToClose.forEach(Post::close);
+        log.info("총 {}개 게시글 마감", postsNeedToClose.size());
     }
 }
