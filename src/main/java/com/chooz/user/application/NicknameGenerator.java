ef157f9dc58018bab0b477a0e919a49ddafa2b39
yunseongoh <gagizzang92@gmail.com>
2025-07-28 01:52:00 +0900

    refactor : 닉네임 생성 방식 수정

--- src/main/java/com/chooz/user/application/NicknameGenerator.java
+++ src/main/java/com/chooz/user/application/NicknameGenerator.java
@@ -1,6 +1,7 @@
 package com.chooz.user.application;
 
 import com.chooz.user.domain.NicknameAdjectiveRepository;
+import com.chooz.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +10,20 @@ import org.springframework.stereotype.Component;
 public class NicknameGenerator {
 
     private final NicknameAdjectiveRepository nicknameAdjectiveRepository;
+    private final UserRepository userRepository;
 
     public String generate() {
-        return nicknameAdjectiveRepository.findRandomNicknameAdjective()
+        String nickName = nicknameAdjectiveRepository.findRandomNicknameAdjective()
                 .map(adjective -> adjective.getAdjective() + " 츄")
                 .orElse("숨겨진 츄");
+        return checkDuplicateNickname(nickName);
+    }
+    private String checkDuplicateNickname(String nickName) {
+        int suffix = 1;
+        while (userRepository.existsByNickname(nickName)){
+            nickName = nickName + suffix;
+            suffix++;
+        }
+        return nickName;
     }
 }
