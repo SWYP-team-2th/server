d8efab7615d7d07da01e6f9ce080eb42791a31cf
wlsh44 <k87913j@naver.com>
2025-09-25 10:09:08 +0900

    fix: 마감 스케줄링 간격 1초로 변경 및 서버 시간 로그 추가

--- src/main/java/com/chooz/post/application/DateCloseScheduler.java
+++ src/main/java/com/chooz/post/application/DateCloseScheduler.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -18,9 +19,9 @@ public class DateCloseScheduler {
     private final PostRepository postRepository;
 
     @Transactional
-    @Scheduled(fixedDelay = 1000 * 60)
+    @Scheduled(fixedDelay = 1000)
     public void closePostsByDate() {
-        log.info("마감 스케줄링 시작");
+        log.info("마감 스케줄링 시작 | 서버 시간: {}", LocalDateTime.now());
         List<Post> postsNeedToClose = postRepository.findPostsNeedToClose();
         postsNeedToClose.forEach(Post::close);
         log.info("총 {}개 게시글 마감", postsNeedToClose.size());
