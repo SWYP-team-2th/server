fb2e94ff64709c05a875124ff6bb588a1d00ae74
yunseongoh <gagizzang92@gmail.com>
2025-10-09 00:36:55 +0900

    test : IntegrationTest transactional import 패키지 수정, 불필요한 import 삭제

--- src/test/java/com/chooz/support/IntegrationTest.java
+++ src/test/java/com/chooz/support/IntegrationTest.java
@@ -1,9 +1,8 @@
 package com.chooz.support;
 
-import jakarta.transaction.Transactional;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @Transactional
