8e9b31c5baf21454ea0669608553491c7f1ff18f
yunseongoh <gagizzang92@gmail.com>
2025-11-03 21:40:55 +0900

    refactor : 에러 메세지 응답 형식 수정

--- src/main/java/com/chooz/common/exception/ErrorResponse.java
+++ src/main/java/com/chooz/common/exception/ErrorResponse.java
@@ -1,8 +1,8 @@
 package com.chooz.common.exception;
 
-public record ErrorResponse(ErrorCode errorCode, String message) {
+public record ErrorResponse(ErrorCode errorCode, String message, String subMessage) {
 
     public static ErrorResponse of(ErrorCode errorCode) {
-        return new ErrorResponse(errorCode, errorCode.getMessage());
+        return new ErrorResponse(errorCode, errorCode.getMessage(), errorCode.getSubMessage());
     }
 }
