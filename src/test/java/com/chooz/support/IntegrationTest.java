477501effa78743a7d5e7cd631aab2c206c61620
wlsh44 <k87913j@naver.com>
2025-11-10 11:34:19 +0900

    test: 테스트 코드 정리 및 s3 mock 추가

--- src/test/java/com/chooz/support/IntegrationTest.java
+++ src/test/java/com/chooz/support/IntegrationTest.java
@@ -1,11 +1,28 @@
 package com.chooz.support;
 
+import com.chooz.image.application.S3Client;
+import com.chooz.support.mock.AwsS3Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+@Import(IntegrationTest.IntegrationTestConfig.class)
 @ActiveProfiles("test")
 @Transactional
 @SpringBootTest
 public abstract class IntegrationTest {
+
+    @Configuration
+    public static class IntegrationTestConfig {
+
+        @Bean
+        @Primary
+        public S3Client s3ClientMock() {
+            return new AwsS3Mock();
+        }
+    }
 }
